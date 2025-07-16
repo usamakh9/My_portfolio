084104d3ff075089312436bc217d0aa477d884cc
usamakh9 <usamakhanonline009@gmail.com>
2025-07-16 12:25:46 +0200

    index id as a separate page

--- django_projects/mysite/main/urls.py
+++ django_projects/mysite/main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("1/", views.v1, name="v1")
+    path("", views.home, name="home"),
+    path("<int:id>/", views.index, name="index")
 ]
 
--- django_projects/mysite/main/views.py
+++ django_projects/mysite/main/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def index(response):
-    return HttpResponse("<h1>My site<h1/>")
 
-def v1(response):
-    return HttpResponse("view 1")
+def home(response):
+    return HttpResponse('<h1>My site</h1>')
+def index(response, id):
+    return HttpResponse("<h1>%s<h1/>" %id)
+
