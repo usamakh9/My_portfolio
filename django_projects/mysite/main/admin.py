1a704d501a8c5cb05269b15198c5d9ae95338865
usamakh9 <usamakhanonline009@gmail.com>
2025-07-16 22:30:35 +0200

    admin and template setup

--- django_projects/mysite/main/admin.py
+++ django_projects/mysite/main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Todolist, Item
 # Register your models here.
+admin.site.register(Todolist)
+admin.site.register(Item)
+
--- django_projects/mysite/main/views.py
+++ django_projects/mysite/main/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Todolist, Item
 # Create your views here.
 
 
 def home(response):
     return HttpResponse('<h1>My site</h1>')
+
 def index(response, id):
-    return HttpResponse("<h1>%s<h1/>" %id)
+    ls = Todolist.objects.get(id=id)
+    items = ls.item_set.get(id=1)
+    return HttpResponse("<h1>%s<h1/><br><br>" %str(items.text))
 
