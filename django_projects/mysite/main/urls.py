7a942aa431170b5d81a7846e2883aacfdf0089a0
usamakh9 <usamakhanonline009@gmail.com>
2025-07-28 10:54:36 +0200

    form can add data and can redirect to id page

--- django_projects/mysite/main/urls.py
+++ django_projects/mysite/main/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("<int:id>/", views.index, name="index")
+    path("<int:id>/", views.index, name="index"),
+    path("create/", views.create, name = "create"),
 ]
 
--- django_projects/mysite/main/views.py
+++ django_projects/mysite/main/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse , HttpResponseRedirect
 from .models import Todolist, Item
+from .form import CreateNewList
+
 # Create your views here.
 
 
 def home(response):
-    return render('response', 'main/home.html', {})
+    return render(response, 'main/home.html', {})
 
 def index(response, id):
     ls = Todolist.objects.get(id=id)
-    return render('response', 'main/list.html', {'ls':ls})
+    return render(response, 'main/list.html', {'ls':ls})
 
+def create(response):
+    if response.method == 'POST':
+        form = CreateNewList(response.POST)
+        if form.is_valid():
+            n = form.cleaned_data["name"]
+            t=Todolist(name=n)
+            t.save()
+    return HttpResponseRedirect('/%i' %t.id)
+    
+    
+    form = CreateNewList()
+    return render(response, 'main/create.html', {'form' : form})
